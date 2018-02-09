9763fcb0bc13a7b3819bec6be2d5c199d318bdf4
rudin23 <rudin_lopez@hotmail.com>
2018-02-08 20:22:18 -0600

    capitulo2\Auto

---
+++ Capitulo2/EjerciciosCap2/Cuadrados.java
@@ -0,0 +1,42 @@
+/*Decimo septimo programa del libro
+  Ejercicio 2.31 capitulo 2
+  Programa para calcular el cuadrado y el cubo de los numeros
+  Autor: Rudin Alexander López Salvatierra
+  Fecha: 13/02/2014
+  Hora: 15:35
+*/
+
+//Inicio de la clase Cuadrados
+public class Cuadrados
+{
+   //Inicio del método main
+   public static void main (String args[])
+   {
+      int x = 0; //Variable que se va a multiplicar
+	  
+	  System.out.println("Nu'mero\t\tCuadrado\tCubo");//Encabezado
+	  
+	  System.out.printf("  %d\t  %d\t %d\n", x, x*x, x*x*x); //Fila 0
+	  x = x + 1;
+	  System.out.printf("  %d\t  %d\t %d\n", x, x*x, x*x*x); //Fila 1
+	  x = x + 1;
+	  System.out.printf("  %d\t  %d\t %d\n", x, x*x, x*x*x); //Fila 2
+	  x = x + 1;
+	  System.out.printf("  %d\t  %d\t %d\n", x, x*x, x*x*x); //Fila 3
+	  x = x + 1;
+	  System.out.printf("  %d\t  %d\t %d\n", x, x*x, x*x*x); //Fila 4
+	  x = x + 1;
+	  System.out.printf("  %d\t  %d\t %d\n", x, x*x, x*x*x); //Fila 5
+	  x = x + 1;
+	  System.out.printf("  %d\t  %d\t %d\n", x, x*x, x*x*x); //Fila 6
+	  x = x + 1;
+	  System.out.printf("  %d\t  %d\t %d\n", x, x*x, x*x*x); //Fila 7
+	  x = x + 1;
+	  System.out.printf("  %d\t  %d\t %d\n", x, x*x, x*x*x); //Fila 8
+	  x = x + 1;
+	  System.out.printf("  %d\t  %d\t %d\n", x, x*x, x*x*x); //Fila 9
+	  x = x + 1;
+	  System.out.printf("  %d\t  %d\t %d\n", x, x*x, x*x*x); //Fila 10
+	  
+   } //Fin del método main
+} //Fin de la clase Cuadrados
